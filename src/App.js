90688b6d72226f510bf1c233eab39d2d24caefcb
Kris Ringler <krisringler@kriss-air.attlocal.net>
2020-09-25 16:48:17 -0700

    gsap added - version 1 complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
+import { TweenMax, Power3 } from 'gsap'
 import './App.css';
 import Modal from './modal';
 
 
 function App() {
+  const video = {
+    url: "http://www.youtube.com/embed/xDMP3i36naA"
+  }
+  // let logoItem = useRef(null)
+  // console.log(logoItem);
+  // useEffect(() => {
+  //   TweenMax.to(
+  //     logoItem,
+  //     .8,
+  //     {
+  //       opacity: 1,
+  //       y: -20,
+  //       ease: Power3.easeOut
+  //     }
+  //   )
+  // }, [])
   return (
     <div className="App">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium debitis, illum, perferendis minus eum saepe commodi quisquam temporibus laborum mollitia quasi recusandae facere repudiandae, repellendus totam ratione! Tempora, sapiente necessitatibus.</p>
-      <p>Ipsum iste at similique explicabo neque hic quas delectus, nulla ab ea aliquid a quo suscipit, voluptas sequi ex et eveniet repellat, aliquam repudiandae cumque! Sunt, commodi. Blanditiis, et incidunt?</p>
-      <p>Eos id culpa quae, labore rerum fuga blanditiis dolor eaque. Magnam dolorem in vitae eligendi quo quis, dolores facilis molestiae aut mollitia eaque quae numquam quod alias sint error amet.</p>
-      <p>Sapiente vero accusamus quasi atque dolorem consequuntur corporis hic! Nam vel voluptates in magni nisi! Facilis expedita perferendis, rem modi optio ratione sapiente quos pariatur voluptas repellat aliquid veniam commodi?</p>
-      <p>Ex ab laudantium voluptatem minima dolorum ut, maxime totam iusto exercitationem molestias, aut, veniam eius ullam repellendus. Incidunt aut animi maiores veniam illo magni accusamus labore eligendi! Reiciendis, et deserunt?</p>
-      <Modal />
+
+      <Modal videoSrc={video.url} />
     </div>
   );
 }
--- src/modal.js
+++ src/modal.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { TweenMax, Power3 } from 'gsap'
+import Iframe from 'react-iframe'
 
-const Modal = () => {
+const Modal = ({ videoSrc }) => {
   const [isOpen, setIsOpen] = useState(false);
+  let overlay = useRef(null);
+  let modal = useRef(null);
+
+  //toggles modal open or closed
+
   function toggle() {
     if (isOpen) {
       setIsOpen(false)
@@ -9,21 +16,44 @@ const Modal = () => {
       setIsOpen(true)
     }
   }
+
+  //componentDidMount animations
+
+  useEffect(() => {
+    const open = document.querySelector('.open');
+    const close = document.querySelector('.close');
+
+    open.addEventListener('click', function () {
+      TweenMax.to(modal, .9, { top: '50%', ease: Power3.easeOut });
+      TweenMax.to(overlay, .9, { backgroundColor: 'rgba(0, 0, 0, 0.5)' })
+    })
+    close.addEventListener('click', function () {
+
+      TweenMax.to(modal, 3.5, { top: '-200%', ease: Power3.easeOut })
+      TweenMax.to(overlay, 3.5, { backgroundColor: 'rgba(0, 0, 0, 0)' })
+        .then(() => toggle());
+    })
+  })
+
   return (
     <>
+      <button onClick={toggle} className="open">Show Modal </button>
+      <div className={`${isOpen ? 'overlay' : ''} `} ref={element => overlay = element}>
+        <div className={`modal ${isOpen ? 'show' : 'hide'}`} ref={element => modal = element} >
+          {/* IFRAME NPM PACKAGE  */}
 
-      <button onClick={toggle}>Show Modal </button>
-      <div className={`${isOpen ? 'overlay' : ''} `}>
+          <Iframe url={videoSrc}
+            width=""
+            height=""
+            id="myId"
+            className="iframe"
+            position="absolute" />
 
-
-
-        <div className={`modal ${isOpen ? 'show overlay' : 'hide'}`} onClose={toggle}>
-          <div className="videoContainer">
-
+          <div className="closeButtonContainer">
+            <button className="close" >X</button>
           </div>
-          <button onClick={toggle} className="closeButton">Close Modal</button>
-        </div>
 
+        </div>
       </div>
     </>
   )
