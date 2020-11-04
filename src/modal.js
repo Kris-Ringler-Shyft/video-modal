0dabfd81a658cf9533655951c00fab90a036b584
Kris Ringler <krisringler@Kriss-MacBook-Air.local>
2020-11-04 13:14:48 -0800

    cleaned up some react code to remove vanilla JS and only using react methods

--- src/modal.js
+++ src/modal.js
@@ -4,42 +4,30 @@ import Iframe from 'react-iframe'
 
 const Modal = ({ videoSrc }) => {
   const [isOpen, setIsOpen] = useState(false);
-  let overlay = useRef(null);
-  let modal = useRef(null);
 
-  //toggles modal open or closed
 
-  function toggle() {
-    if (isOpen) {
-      setIsOpen(false)
-    } else {
-      setIsOpen(true)
+  const closeModal = () => {
+    TweenMax.to('.modal', 3, { top: '-150%', ease: Power3.easeOut })
+    TweenMax.to('.overlay', 3, { backgroundColor: 'rgba(0, 0, 0, 0)' })
+    const timeout = setTimeout(() => {
+      setIsOpen(false);
+
+    }, 1000);
+    return () => {
+      clearTimeout(timeout);
     }
+
   }
-
-  //componentDidMount animations
-
-  useEffect(() => {
-    const open = document.querySelector('.open');
-    const close = document.querySelector('.close');
-
-    open.addEventListener('click', function () {
-      TweenMax.to(modal, .9, { top: '50%', ease: Power3.easeOut });
-      TweenMax.to(overlay, .9, { backgroundColor: 'rgba(0, 0, 0, 0.5)' })
-    })
-    close.addEventListener('click', function () {
-
-      TweenMax.to(modal, 3.5, { top: '-200%', ease: Power3.easeOut })
-      TweenMax.to(overlay, 3.5, { backgroundColor: 'rgba(0, 0, 0, 0)' })
-        .then(() => toggle());
-    })
-  })
 
   return (
     <>
-      <button onClick={toggle} className="open">Show Modal </button>
-      <div className={`${isOpen ? 'overlay' : ''} `} ref={element => overlay = element}>
-        <div className={`modal ${isOpen ? 'show' : 'hide'}`} ref={element => modal = element} >
+      <button onClick={() => {
+        setIsOpen(true)
+        TweenMax.to('.modal', .9, { top: '50%', ease: Power3.easeOut });
+        TweenMax.to('.overlay', 1, {backgroundColor: 'rgba(0, 0, 0, 0.5'})
+      }}>Show Modal </button>
+      <div className={`${isOpen ? 'overlay' : ''} `} >
+        <div className={`modal ${isOpen ? 'show' : 'hide'}`} >
           {/* IFRAME NPM PACKAGE  */}
 
           <Iframe url={videoSrc}
@@ -50,9 +38,8 @@ const Modal = ({ videoSrc }) => {
             position="absolute" />
 
           <div className="closeButtonContainer">
-            <button className="close" >X</button>
+            <button onClick={closeModal}>X</button>
           </div>
-
         </div>
       </div>
     </>
